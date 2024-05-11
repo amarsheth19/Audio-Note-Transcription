13c499ebb7300f2f143eb680775f952e9d2a30f9
amarsheth19 <amar.sheth19@gmail.com>
2024-05-10 23:01:32 -0400

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request
+#from openai import OpenAI
+
+import time
 
 app = Flask(__name__)
 
@@ -10,7 +13,38 @@ def members():
 @app.route("/") 
 def index(): 
     print("in index")
-    return "Homepage of GeeksForGeeks"
+    return "Homepage of Website"
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
+@app.route("/username", methods=['POST'])
+def login():
+    username = request.json["username"]
+    #username = request.args.get('username')
+    return {"username": 'This is the backend -- >'+ username }
+
+@app.route('/upload', methods = ['POST'])
+def upload_file():
+    try:
+        file = request.files['file']
+        #client = OpenAI()
+        audio_file = open(file, "rb")
+        transcription = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file,
+        response_format="text"
+        )
+        print(transcription)
+        submit = "Take notes on the following lecture: " + transcription
+        response = client.completions.create(
+        model="gpt-3.5-turbo-0125",
+        prompt= submit
+        )
+        return response
+    except:
+        return "file sent to flask but transciption caused an error"
 
 if __name__ == "__main__":
     app.run(debug=True) 
