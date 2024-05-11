5667f4730c161328bb34bc78b76ce7a4a79ebf4a
amarsheth19 <amar.sheth19@gmail.com>
2024-05-10 23:05:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
-  const [data, setData] = useState([{}]);
-  var output;
+ 
+ const [file, setFile] = useState(null);
+ const [msg, setMsg] = useState(null);
+ const [progress, setProgress] =useState({started: false, pc: 0});
+
+  
+
+function handleUpload(){
+  if(!file){
+    setMsg("No file selected");
+    return;
+  }
+
+  const fd = new FormData();
+  fd.append('file', file);
+  
+  setMsg("Uploading...");
+  setProgress(prevState => {
+    return {...prevState, started:true}
+  })
+
+  //To see file contents in console
+  axios.post('http://httpbin.org/post', fd, {
+    onUploadProgress: (ProgressEvent) => { setProgress(prevState => {
+      return {...prevState, pc: ProgressEvent.progress*100}
+    })},
+  })
+  .then(res => {
+    setMsg("Upload Successful");
+    console.log(res.data);
+  })
+  .catch(err => {
+    setMsg("Upload Unsuccessful");
+    console.error(err);
+  });
 
 
-console.log("here1")
+  //sending to flask backend
+  axios.post('/upload', fd, {
+    onUploadProgress: (ProgressEvent) => { setProgress(prevState => {
+      return {...prevState, pc: ProgressEvent.progress*100}
+    })},
+  })
+  .then(res => {
+    setMsg("Upload Successful");
+    console.log("Data: " + res.data);
+  })
+  .catch(err => {
+    setMsg("Upload Unsuccessful");
+    console.error(err);
+  });
 
-  /**useEffect(() => {
 
-    fetch('/members').then(
-      res => res.json()
-    ).then(data => {console.log(data)})
-  });*/
+
+}
 
   return(
-    <div>
+  
+    <div className="App">
+      <h1>Uploading Files in React</h1>
+      <input onChange={(e) => setFile(e.target.files[0])} type="file"/>
+      <button onClick={handleUpload}>Upload</button>
+      <p></p>
+      {msg && <span>{msg}</span>}
+      <p></p>
+      {progress.started && <progress max = "100" value = {progress.pc}></progress>}
+
+    
     </div>
+
+
   )
 }
 
 export default App
-
 
