143d70aea7beeb6f6c3aca7ebd58c4a08fe5816c
amarsheth19 <amar.sheth19@gmail.com>
2024-05-10 20:02:15 -0400

    Sending Files Between React and Flask

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
-  const [data, setData] = useState([{}]);
-  var output;
+ 
+ const [file, setFile] = useState(null);
+ const [msg, setMsg] = useState(null);
+ const [progress, setProgress] =useState({started: false, pc: 0});
+
+  
+
+function handleUpload(){
+  if(!file){
+    setMsg("No file selected");
+    return;
+  }
+
+  const fd = new FormData();
+  fd.append('file', file);
+  
+  setMsg("Uploading...");
+  setProgress(prevState => {
+    return {...prevState, started:true}
+  })
+
+  //To see file contents in console
+  axios.post('http://httpbin.org/post', fd, {
+    onUploadProgress: (ProgressEvent) => { setProgress(prevState => {
+      return {...prevState, pc: ProgressEvent.progress*100}
+    })},
+  })
+  .then(res => {
+    setMsg("Upload Successful");
+    console.log(res.data);
+  })
+  .catch(err => {
+    setMsg("Upload Unsuccessful");
+    console.error(err);
+  });
 
 
-console.log("here1")
+  //sending to flask backend
+  axios.post('/upload', fd, {
+    onUploadProgress: (ProgressEvent) => { setProgress(prevState => {
+      return {...prevState, pc: ProgressEvent.progress*100}
+    })},
+  })
+  .then(res => {
+    setMsg("Upload Successful");
+    console.log("Data: " + res.data);
+  })
+  .catch(err => {
+    setMsg("Upload Unsuccessful");
+    console.error(err);
+  });
 
+<<<<<<< Updated upstream
   useEffect(() => {
 
     fetch('/members').then(
@@ -28,10 +77,27 @@ console.log("here1")
       output
 
 
+=======
+}
+
+  return(
+  
+    <div className="App">
+      <h1>Uploading Files in React</h1>
+      <input onChange={(e) => setFile(e.target.files[0])} type="file"/>
+      <button onClick={handleUpload}>Upload</button>
+      <p></p>
+      {msg && <span>{msg}</span>}
+      <p></p>
+      {progress.started && <progress max = "100" value = {progress.pc}></progress>}
+
+      
+>>>>>>> Stashed changes
     </div>
+
+
   )
 }
 
 export default App
-
 
