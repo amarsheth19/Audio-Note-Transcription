5e28fbe0eb0451f8cf4b68d36cc2c69a1a7a96d6
amarsheth19 <amar.sheth19@gmail.com>
2024-05-16 11:39:09 -0400

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -1,23 +1,34 @@
-from flask import Flask, request
-#from openai import OpenAI
-
+from flask import Flask, request, session
+from openai import OpenAI
+import os
+#import pyrebase
+from werkzeug.utils import secure_filename
 import time
+import send2trash
 
+
+UPLOAD_FOLDER = 'C:/Users/AmarS/CapstoneFinalProject/Capstone-Final-Project/Flask'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+transcribed_files = {}
+"""firebaseConfig = {
+  'apiKey': "AIzaSyAoHqmKxapjNj2_KI1PCwmYEpBhDzplwqI",
+  'authDomain': "note-taker-b550d.firebaseapp.com",
+  'projectId': "note-taker-b550d",
+  'storageBucket': "note-taker-b550d.appspot.com",
+  'messagingSenderId': "508381657343",
+  'appId': "1:508381657343:web:49f24213e68cf0693a74ca",
+  'measurementId': "G-ZB5LKXBKZ7"
+}
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
 
-@app.route('/members')
-def members():
-    print("in members")
-    return {"username": "username"}
 
-@app.route("/") 
-def index(): 
-    print("in index")
-    return "Homepage of Website"
-
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/signup')
+def signup():
+    email = input("Enter email: ")
+    password = input("Enter password: ")
+    user = auth.create_user_with_email_and_password(email,password)"""
 
 @app.route("/username", methods=['POST'])
 def login():
@@ -27,24 +38,31 @@ def login():
 
 @app.route('/upload', methods = ['POST'])
 def upload_file():
-    try:
-        file = request.files['file']
-        #client = OpenAI()
-        audio_file = open(file, "rb")
-        transcription = client.audio.transcriptions.create(
+    target=os.path.join(UPLOAD_FOLDER,'audio_files')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    file = request.files['file']
+    id_token = request.form['id_token']
+    #filename = secure_filename(file.filename)
+    filename = "audiofile.mp3"
+    destination="/".join([target, filename])
+    file.save(destination)
+    #
+    audio_file = open("Flask/audio_files/audiofile.mp3", "rb")
+    transcription = client.audio.transcriptions.create(
         model="whisper-1",
         file=audio_file,
         response_format="text"
-        )
-        print(transcription)
-        submit = "Take notes on the following lecture: " + transcription
-        response = client.completions.create(
+    )
+    transcribed_files[secure_filename(file.filename)] = transcription
+    return transcription
+    """submit = "Take notes on the following lecture: " + transcription
+    response = client.completions.create(
         model="gpt-3.5-turbo-0125",
         prompt= submit
-        )
-        return response
-    except:
-        return "file sent to flask but transciption caused an error"
+    )
+    return response"""
+
 
 if __name__ == "__main__":
     app.run(debug=True) 
